0dc54479d034d9c500f8c1cb23b99e12ea0b5340
Sergio Rodrigues dos Santos <sergiotinfo3@gmail.com>
2024-01-26 12:29:20 -0300

    CommitInicial2

--- src/CarroRobo/OperaCarro.java
+++ src/CarroRobo/OperaCarro.java
@@ -1,20 +1,6 @@
 package CarroRobo;
 
-//===========================================================//
-//turn: Virar
-//direction: direção
-//radius: raio
-//startspeed: velocidade inicial
-//endspeed: velocidade final
-//changelanes: mudar de faixa
-//signalturn: sinal virar
-//signalOn: sinalizar ligado
-//radarTurn: giro dor radar
-//radarRear: radar traseiro
-//radarFront: radar frontal
-//speedOfCar: velocidade do carro
-//distanciaToCar: distancia do carro
-//==========================================================//
+
 public interface OperaCarro {
 
     default int Virar(Direcao direcao,
